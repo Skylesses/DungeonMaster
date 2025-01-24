892d07177a81b3b5cd64fa5e4f838cf9eab026e3
Michael <m.dumor17451@hs-ansbach.de>
2025-01-24 17:21:04 +0100

    some stuff working, some not so much

--- DungeonMaster/Assets/Scripts/DragNDrop.cs
+++ DungeonMaster/Assets/Scripts/DragNDrop.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class DragNDrop : MonoBehaviour
 {
+    //obj's for dragging and dropping
+    public GameObject dragObj;
+    //public GameObject dragPos;
+    //public float dropDistance;
+
+    public bool mouseDown;
+
     private Vector3 mousePos;
 
     private Vector3 GetMousePos()
@@ -14,10 +21,25 @@ public class DragNDrop : MonoBehaviour
     private void OnMouseDown()
     {
         mousePos = Input.mousePosition - GetMousePos();
+        mouseDown = true;
     }
-
+    //move with mouse drag
     private void OnMouseDrag()
     {
         transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePos);
+    }
+
+    private void OnMouseUp()
+    {
+        mouseDown = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(dragObj.tag) /*&& mouseDown == false*/)
+        {
+            dragObj.transform.position = other.transform.position;
+            Debug.Log(other.name);
+        }
     }
 }
