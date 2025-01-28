29fada6da7311239bafcef659a5f15def8cfb97b
Michael <m.dumor17451@hs-ansbach.de>
2025-01-28 17:25:04 +0100

    Drag and Drop funzt endlich richtig
    nur noch Spezialfälle wie Waffe an Monsterhand, Waffe bewegt sich noch net mit Monster wenn Monster bewegt wird

--- DungeonMaster/Assets/Scripts/ClampToScreen.cs
+++ DungeonMaster/Assets/Scripts/ClampToScreen.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class ClampToScreen : MonoBehaviour
 {
-    public float yPos = 4;
-    public float xPos = 8;
+    public Camera cam;
+    private Vector3 screenBounds;
 
+    private float objWidth;
+    private float objHeight;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        screenBounds = cam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, cam.transform.position.z));
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        objWidth = sr.bounds.extents.x;
+        objHeight = sr.bounds.extents.y;
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -xPos, xPos),
-            Mathf.Clamp(transform.position.y, -yPos, yPos), transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -screenBounds.x + objWidth, screenBounds.x - objWidth),
+            Mathf.Clamp(transform.position.y, -screenBounds.y + objHeight, screenBounds.y - objHeight), transform.position.z);
     }
 }
--- DungeonMaster/Assets/Scripts/DragNDrop.cs
+++ DungeonMaster/Assets/Scripts/DragNDrop.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DragNDrop : MonoBehaviour
-{
-    public GameObject dragObj;
+public class DragNDrop : MonoBehaviour,  IPointerClickHandler
+{   
     public GameObject dropPos;
+    private GameObject dragObj;
 
     public float dropDistance;
 
+    private bool isLocked;
+
+    Vector3 objStartPos;
+
+    private void Start()
+    {
+        dragObj = this.gameObject;
+        objStartPos = dragObj.transform.position;
+        isLocked = false;
+    }
+
     public void DragObj()
     {
-        dragObj.transform.position = Input.mousePosition;
+        if(!isLocked)
+        {
+            dragObj.transform.position = Input.mousePosition;
+        }
+       
     }
 
     public void DropObj()
@@ -19,7 +35,21 @@ public class DragNDrop : MonoBehaviour
         float Distance = Vector3.Distance(dragObj.transform.position, dropPos.transform.position);
         if(Distance < dropDistance)
         {
+            isLocked = true;
             dragObj.transform.position = dropPos.transform.position;
+        }
+        else
+        {
+            dragObj.transform.position = objStartPos;
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {  
+        if(eventData.button == PointerEventData.InputButton.Right)
+        {
+            dragObj.transform.position = objStartPos;
+            isLocked = false;
         }
     }
 }
