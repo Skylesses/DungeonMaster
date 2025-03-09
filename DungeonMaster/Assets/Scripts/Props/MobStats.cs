f8a60b88b6afaa29d7d24cfe733fed3e28143203
Michael <m.dumor17451@hs-ansbach.de>
2025-03-09 16:38:56 +0100

    weapon attach logic

--- DungeonMaster/Assets/Scripts/Props/MobStats.cs
+++ DungeonMaster/Assets/Scripts/Props/MobStats.cs
@@ -10,6 +10,10 @@ public class MobStats : MonoBehaviour
     public static bool bow;
     public static bool shield;
 
+    //Drag N Drop script
+    public GameObject weapon;
+    public DragNDropWeapons dragScript;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +22,82 @@ public class MobStats : MonoBehaviour
         spear = false;
         bow = false;
         shield = false;
+
+        //get Drag N Drop script
+        weapon = this.gameObject;
+        dragScript = weapon.GetComponent<DragNDropWeapons>();
     }
 
+    void Update()
+    {
+        if(dragScript.isLocked == true)
+        {
+            AttachWeapon();
+        }
 
+        if(dragScript.isLocked == false)
+        {
+            DetachWeapon();
+        }
+    }
+
+    //set bool depending on weapon type when weapon attached
+    public void AttachWeapon()
+    {
+        if(weapon.tag == "sword")
+        {
+            sword = true;
+            Debug.Log("Sword!");
+        }
+        if (weapon.tag == "club")
+        {
+            club = true;
+            Debug.Log("Club!");
+        }
+        if (weapon.tag == "spear")
+        {
+            spear = true;
+            Debug.Log("Spear!");
+        }
+        if (weapon.tag == "bow")
+        {
+            bow = true;
+            Debug.Log("Bow!");
+        }
+        if (weapon.tag == "shield")
+        {
+            shield = true;
+            Debug.Log("Shield!");
+        }
+    }
+
+    //set bool depending on weapon type when weapon detached
+    public void DetachWeapon()
+    {
+        if(weapon.tag == "sword")
+        {
+            sword = false;
+            Debug.Log("No sword!");
+        }
+        if (weapon.tag == "club")
+        {
+            club = false;
+            Debug.Log("No club!");
+        }
+        if (weapon.tag == "spear")
+        {
+            spear = false;
+            Debug.Log("No spear!");
+        }
+        if (weapon.tag == "bow")
+        {
+            bow = false;
+            Debug.Log("No bow!");
+        }
+        if (weapon.tag == "shield")
+        {
+            shield = false;
+            Debug.Log("No shield!");
+        }
+    }
 }
