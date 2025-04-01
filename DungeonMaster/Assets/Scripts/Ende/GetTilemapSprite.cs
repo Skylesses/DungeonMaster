a44fabf4edcf1de43797e2832aeade19307926f6
Michael <m.dumor17451@hs-ansbach.de>
2025-04-01 16:04:31 +0200

    .

--- DungeonMaster/Assets/Scripts/Ende/GetTilemapSprite.cs
+++ DungeonMaster/Assets/Scripts/Ende/GetTilemapSprite.cs
@@ -7,9 +7,9 @@ public class GetTilemapSprite : MonoBehaviour
 {
     public Image image;
 
-
     private void Start()
     {
+        //get sprite from tilemap in Map Scene
         if(MapSprite.GlobalSpriteStorage.savedSprite != null)
         {
             image.sprite = MapSprite.GlobalSpriteStorage.savedSprite;
