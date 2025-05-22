10246bd039008a4ab3ff5ada833fb7309fcd8e1c
Michael <m.dumor17451@hs-ansbach.de>
2025-05-22 14:32:23 +0200

    Ending Stuff

--- DungeonMaster/Assets/Scripts/Ende/EndingManager.cs
+++ DungeonMaster/Assets/Scripts/Ende/EndingManager.cs
@@ -3,41 +3,141 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EndingManager : MonoBehaviour
-{
-    public GameObject tavern;
-    public GameObject ship;
-    public GameObject jail;
-    public GameObject wood;
+{   
+    [Header("OneShots")]
+    public GameObject tavernOS;
+    public GameObject shipOS;
+    public GameObject jailOS;
+    public GameObject woodsOS;
+
+    [Header("Props")]
+    public GameObject tavernOne;
+    public GameObject shipOne;
+    public GameObject jailOne;
+    public GameObject woodsOne;
+    public GameObject tavernTwo;
+    public GameObject shipTwo;
+    public GameObject jailTwo;
+    public GameObject woodsTwo;
+
+    [Header("Maps")]
+    public GameObject mapOne;
+    public GameObject mapTwo;
+
+    [Header("Save Buttons")]
+    public GameObject saveOneShot;
+    public GameObject savePropsOne;
+    public GameObject savePropsTwo;
+    public GameObject saveMapOne;
+    public GameObject saveMapTwo;
 
     // Start is called before the first frame update
     void Start()
     {
-        tavern.SetActive(false);
-        ship.SetActive(false);
-        jail.SetActive(false);
-        wood.SetActive(false);
+        tavernOS.SetActive(false);
+        shipOS.SetActive(false);
+        jailOS.SetActive(false);
+        woodsOS.SetActive(false);
 
+        tavernOne.SetActive(false);
+        shipOne.SetActive(false);
+        jailOne.SetActive(false);
+        woodsOne.SetActive(false);
+
+        tavernTwo.SetActive(false);
+        shipTwo.SetActive(false);
+        jailTwo.SetActive(false);
+        woodsTwo.SetActive(false);
+
+        saveOneShot.SetActive(true);
+        savePropsOne.SetActive(false);
+        savePropsTwo.SetActive(false);
+        saveMapOne.SetActive(false);
+        saveMapTwo.SetActive(false);
+
+        //show One Shot depending on story selection
         if (StorySelection.tavern == true)
         {
-            tavern.SetActive(true);
+            tavernOS.SetActive(true);
         }
         if (StorySelection.ship == true)
         {
-            ship.SetActive(true);
+            shipOS.SetActive(true);
         }
         if (StorySelection.jail == true)
         {
-            jail.SetActive(true);
+            jailOS.SetActive(true);
         }
         if (StorySelection.woods == true)
         {
-            wood.SetActive(true);
+            woodsOS.SetActive(true);
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    //get next to Props
+    public void changeToPropsOne()
     {
-        
+        tavernOS.SetActive(false);
+        shipOS.SetActive(false);
+        jailOS.SetActive(false);
+        woodsOS.SetActive(false);
+
+        if (StorySelection.tavern == true)
+        {
+            tavernOne.SetActive(true);
+        }
+        if (StorySelection.ship == true)
+        {
+            shipOne.SetActive(true);
+        }
+        if (StorySelection.jail == true)
+        {
+            jailOne.SetActive(true);
+        }
+        if (StorySelection.woods == true)
+        {
+            woodsOne.SetActive(true);
+        }
+    }
+
+    public void changeToPropsTwo()
+    {
+        tavernOne.SetActive(false);
+        shipOne.SetActive(false);
+        jailOne.SetActive(false);
+        woodsOne.SetActive(false);
+
+        if (StorySelection.tavern == true)
+        {
+            tavernTwo.SetActive(true);
+        }
+        if (StorySelection.ship == true)
+        {
+            shipTwo.SetActive(true);
+        }
+        if (StorySelection.jail == true)
+        {
+            jailTwo.SetActive(true);
+        }
+        if (StorySelection.woods == true)
+        {
+            woodsTwo.SetActive(true);
+        }
+    }
+
+    public void changeToMapOne()
+    {
+        tavernTwo.SetActive(false);
+        shipTwo.SetActive(false);
+        jailTwo.SetActive(false);
+        woodsTwo.SetActive(false);
+
+        mapOne.SetActive(true);
+    }
+
+    public void changeToMapTwo()
+    {
+        mapOne.SetActive(false);
+        mapTwo.SetActive(true);
     }
 }
--- DungeonMaster/Assets/Scripts/Ende/GetTilemapSprite2.cs
+++ DungeonMaster/Assets/Scripts/Ende/GetTilemapSprite2.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using SFB;
 
 public class GetTilemapSprite2 : MonoBehaviour
 {
     public Image map2;
+    public GameObject saveButton;
 
     private void Start()
     {
@@ -13,6 +16,40 @@ public class GetTilemapSprite2 : MonoBehaviour
         if(MapSprite2.GlobalSpriteStorage2.savedSprite2 != null)
         {
             map2.sprite = MapSprite2.GlobalSpriteStorage2.savedSprite2;
+        }
+    }
+
+    public void SaveSpriteToDisk()
+    {
+        if (MapSprite2.GlobalSpriteStorage2.savedSprite2 == null)
+        {
+            Debug.LogWarning("No sprite to save!");
+            return;
+        }
+
+        //get texture from sprite
+        Sprite sprite = MapSprite2.GlobalSpriteStorage2.savedSprite2;
+        Texture2D texture = sprite.texture;
+
+        // Optional: Crop to the sprite's rect
+        Rect rect = sprite.rect;
+        Texture2D croppedTexture = new Texture2D((int)rect.width, (int)rect.height, TextureFormat.RGBA32, false);
+        Color[] pixels = texture.GetPixels((int)rect.x, (int)rect.y, (int)rect.width, (int)rect.height);
+        croppedTexture.SetPixels(pixels);
+        croppedTexture.Apply();
+
+        //open file dialog to choose path
+        var path = StandaloneFileBrowser.SaveFilePanel("Save Sprite", "", "MapImage", "png");
+
+        if (!string.IsNullOrEmpty(path))
+        {
+            byte[] bytes = croppedTexture.EncodeToPNG();
+            File.WriteAllBytes(path, bytes);
+            Debug.Log("Saved image to: " + path);
+        }
+        else
+        {
+            Debug.Log("Save cancelled.");
         }
     }
 }
