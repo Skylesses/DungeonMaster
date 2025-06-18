84eb625704459efe8df03f9da43358d3f1b7f98c
Michael <m.dumor17451@hs-ansbach.de>
2025-06-18 19:28:07 +0200

    Credits

---
+++ DungeonMaster/Assets/Scripts/Credits.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    public string mainMenu;
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(mainMenu);
+    }
+}
