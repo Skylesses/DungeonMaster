20c35623e4c478c28cfa87493537a693b7beda05
Michael <m.dumor17451@hs-ansbach.de>
2025-06-18 18:41:25 +0200

    Menu stuff

--- DungeonMaster/Assets/Scripts/MainMenu.cs
+++ DungeonMaster/Assets/Scripts/MainMenu.cs
@@ -6,9 +6,37 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public string nextScene;
+    public string credits;
+
+    public GameObject controlsMenu;
+
+    private void Start()
+    {
+        controlsMenu.SetActive(false);
+    }
 
     public void LoadNextScene()
     {
         SceneManager.LoadScene(nextScene);
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene(credits);
+    }
+
+    public void ShowControls()
+    {
+        controlsMenu.SetActive(true);
+    }
+
+    public void CloseControls()
+    {
+        controlsMenu.SetActive(false);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
     }
 }
